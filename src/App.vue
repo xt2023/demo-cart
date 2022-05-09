96e5b568c20a6eefaefc73d464e7caf98d0a9f4a
xt2023 <2639695678@qq.com>
2022-05-09 23:14:41 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,43 @@
 <template>
   <div class="app-container">
-    <h1>App 根组件</h1>
+  <!-- header模块-->
+    <Header></Header>
+    <Goods v-for="item in list" :key="item.id"></Goods>
   </div>
 </template>
 
 <script>
-export default {}
+//导入axios请求库
+import axios from 'axios'
+//导入需要的组件
+import Header from "@/components/Header/Header.vue";
+import Goods from "@/components/Goods/Goods.vue";
+export default {
+  data(){
+    return {
+      //创建的空数组用来存放请求过来的数据
+      list:[]
+    }
+  },
+  components :{
+    Header,
+    Goods
+  },
+  methods :{
+    async initCatList (){
+      //调用axios的get方法请求数据
+      const {data:res}=await axios.get('https://www.escook.cn/api/cart')
+      console.log(res)
+      this.list=res.list
+
+    }
+  },
+
+  created() {
+    //调用请求数据的方法
+    this.initCatList()
+  }
+}
 </script>
 
 <style lang="less" scoped>
